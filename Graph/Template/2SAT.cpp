70c4a30c7b7a2f8e1c6fcd9add3704962f4648a7
Kimbbakar <chowdhuryosman04@gmail.com>
2016-09-15 10:12:20 +0600

    update 2SAT template

--- Graph/Template/2SAT.cpp
+++ Graph/Template/2SAT.cpp
@@ -1,98 +1,59 @@
-#define MAX 2005
+#define MAX 16006
 
-int nod(int x)
-{
-    if(x<0)
-    {
-        return abs(x)+1000;
-    }
-    return x;
-}
 
 vector<int>lst[MAX];
 vector<int>lst1[MAX];
 int n,m,k;
 int u,v;
+int s[MAX];
 
 int visit[MAX];
-int mark[MAX];
-int col[MAX];
-int taken[MAX];
-stack<int>st;
-int id,cl;
+int id;
 
-int scc(int p)
+int dfs(int p)
 {
+    if(visit[p^1] ) return 0;
+    if(visit[p] ) return 1;
+
     visit[p]=1;
-    mark[p]=id;
-    int mn=id++;
-    st.push(p);
+    s[id++]=p;
 
     for(int j=0;j<sz(lst[p] );j++)
     {
         int i=lst[p][j];
-        if(visit[i]==0)
-        {
-            mn=min(mn,scc(i) );
-        }
-        else if(visit[i]==1)
-        {
-            mn=min(mn,mark[i] );
-        }
-    }
 
-    visit[p]=2;
-
-    if(mn==mark[p])
-    {
-        while(st.top()!=p)
-        {
-            col[st.top()]=cl;
-            lst1[cl].pb(st.top());
-            st.pop();
-        }
-        lst1[cl].pb(st.top());
-        col[st.top()]=cl;
-        st.pop();
-
-        cl++;
+        if(!dfs(i)) return 0;
 
     }
 
-    return mark[p]=mn;
+    return 1;
 }
 
-bool found;
+
 vector<int>res;
 
-bool solve(int x)
+bool solve()
 {
-    id=1;
-    cl=1;
-
-    for(int i=0;i<MAX;i++)
-    {
-        lst1[i].clear();
-        lst2[i]=lst[i] ;
-        mark[i]=0;
-        visit[i]=0;
-        col[i]=0;
-    }
-
     bool ok=false;
 
-    for(int i=1;i<=m;i++)
+    for(int i=0;i<2*m && !ok;i++)
     {
+        id=0;
         if(visit[i]==0)
-            scc(i);
-    }
-
-    for(int i=1;i<=m && !ok;i++)
-    {
-        if(col[i]==col[nod(-i) ] )
         {
-            ok=true;
+            if(!dfs(i))
+            {
+                for(int j=0;j<id;j++)
+                {
+                    visit[s[j] ]=visit[s[j]^1 ]=0;
+                }
+                id=0;
+
+                if(!dfs(i^1) )
+                    ok=true;
+            }
         }
+
     }
 
     return !ok;
@@ -100,26 +61,9 @@ bool solve(int x)
 
 void print()
 {
-    for(int i=1;i<cl ;i++)
+    for(int i=0;i<2*m ;i++)
     {
-        for(int j=0;j<sz(lst1[i] );j++)
-        {
-            int k=lst1[i][j];
-
-            if(taken[k]) continue;
-
-            if(k<=1000 )
-            {
-                taken[k]=1;
-                taken[k+1000]=1;
-                res.pb(k);
-                cnt++;
-            }
-            else
-            {
-                taken[k]=1;
-                taken[k-1000]=1;
-            }
-        }
+        if(visit[i] )
+            res.pb(i);
     }
 }
